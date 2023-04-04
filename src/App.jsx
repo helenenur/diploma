d61dfc310c2eed4a51ee5630f4c9c5aef750ca41
helenenur <helenenur95@gmail.com>
2023-04-04 16:20:15 +0600

    Added basic routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
       <p>This is my diploma.</p>
+
+<nav>
+      <NavLink to ="/">Home</NavLink>
+      <NavLink to ="/about">About</NavLink>
+      </nav>
+
+
+      <main>
+        <Routes>
+          <Route path="/" element={<h2>Home</h2>} exact/>
+          <Route path="/about" element={<h2>About</h2>} />
+        </Routes>
+      </main>
     </div>
   );
 }
