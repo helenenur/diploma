58eddd187e66e114ae1b6436f13d533af66d4fe9
helenenur <helenenur95@gmail.com>
2023-04-11 16:17:31 +0600

    Add CategoryList for aside

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,21 +1,21 @@
-import Nav from"../Nav/Nav";
+import Nav from "../Nav/Nav";
+import Logo from "../Logo/Logo";
 import "./Layout.css";
+import Category from "../../pages/Category";
+import CategoryList from "../CategoryList/CategoryList";
 
-export default function Layout(props){
-  return(
+export default function Layout(props) {
+  return (
     <div className="Layout">
-     <header>
-      <Nav/>
-     </header>
-     <aside>
-      ASIDE
-     </aside>
-    <main>
-  {props.children}
-</main>
-<footer>
-  Footer
-</footer>
- </div>
+      <header>
+        <Logo />
+        <Nav />
+      </header>
+      <aside>
+        <CategoryList />
+      </aside>
+      <main>{props.children}</main>
+      <footer>Footer</footer>
+    </div>
   );
 }
