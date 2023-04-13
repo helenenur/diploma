a1eae4e2431f497e6c92adb3678c6378d5baa327
helenenur <helenenur95@gmail.com>
2023-04-13 14:45:00 +0600

    from CategoryList usesate cuted and pasted in Appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contacts from "./pages/Contacts";
 import Delivery from "./pages/Delivery";
-import Category from"./pages/Category";
+import Category from "./pages/Category";
+import { createContext, useEffect, useState } from "react";
+import { categoryCollection } from "./firebase";
+import { getDocs } from "firebase/firestore";
+
+export const AppContext = createContext({
+  categories: [],
+});
 
 export default function App() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getDocs(categoryCollection).then((snapshot) => {
+      const newCategories = [];
+
+      snapshot.docs.forEach((doc) => {
+        const category = doc.data();
+        category.id = doc.id;
+
+        newCategories.push(category);
+      });
+
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <Layout>
-         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/contacts" element={<Contacts/>} />
-          <Route path="/delivery" element={<Delivery/>} />
-          <Route path="/category/:path" element={<Category/>}/>
-        </Routes>
-      </Layout>
- </div>
+      <AppContext.Provider value={{ categories }}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/category/:path" element={<Category />} />
+          </Routes>
+        </Layout>
+      </AppContext.Provider>
+    </div>
   );
 }
--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,32 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import "./CategoryList.css";
-import { getDocs } from "firebase/firestore";
-import { categoryCollection } from "../../firebase";
 import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
 
 export default function CategoryList() {
-  const [categories, setCategorories] = useState([]);
-
-  useEffect(() => {
-    getDocs(categoryCollection).then((snapshot) => {
-      const newCategories = [];
-
-      snapshot.docs.forEach((doc) => {
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-      });
-
-      setCategorories(newCategories);
-    });
-  }, []);
+  const { categories } = useContext(AppContext);
 
   const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
     </li>
   ));
+
   return (
     <div className="CategoryList">
       <ul>{output}</ul>
