998379312bbff293b16905d4043d84ce890f2cb9
helenenur <helenenur95@gmail.com>
2023-04-13 15:08:32 +0600

    Show category name

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { useMatch } from "react-router-dom";
+import { AppContext } from "../App";
+
 export default function Category() {
+  const { params } = useMatch("/category/:path");
+  const { categories } = useContext(AppContext);
+  const category = categories.find((category) => params.path === category.path);
   return (
     <div className="Category">
-      <h1>Category</h1>
+      <h1>{category.name}</h1>
     </div>
   );
 }
