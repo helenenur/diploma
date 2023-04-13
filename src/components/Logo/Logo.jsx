766c6e884a1e76b789123b1c74bf2eab3da699bb
helenenur <helenenur95@gmail.com>
2023-04-13 14:46:41 +0600

    Move database fetch to App component

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo(){
   return (
     <div className="Logo">
        <NavLink to="/">
-       <img src={logo} alt="Website logo"/>
+       <img src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/40-Famous-Shoe-Logos/Nike.png "alt="Website logo"/>
        </NavLink>
     </div>
   );
