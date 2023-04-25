9523ada26958ff2b7082a7cb2a9a675b24fec77d
helenenur <helenenur95@gmail.com>
2023-04-25 14:59:32 +0600

    Added OrderList

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,13 @@ import {
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import Product from "./pages/Product";
+import ThankYou from "./pages/ThankYou";
+import Orders from "./pages/Order";
 
 export const AppContext = createContext({
   categories: [],
   products: [],
+  orders:[],
 
   // корзина
   cart: {},
@@ -27,6 +30,7 @@ export const AppContext = createContext({
 export default function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [orders, setOrders]= useState([ ]);
 
   // состояние которое хранит информацию пользователя
   const [user, setUser] = useState(null);
@@ -81,14 +85,37 @@ export default function App() {
       setProducts(newProducts);
     });
 
+      // задать новый массив как состояние комапо
+      setCategories(newCategories);
+    });
+
+    // получить продукты из списка продуктов
+    getDocs(productCollection).then((snapshot) => {
+      // продукты будут храниться в snapshot.docs
+
+      // создать массив для продуктов
+      const newProducts = [];
+      // заполнить массив данными из списка продвук
+      snapshot.docs.forEach((doc) => {
+        // doc = продукт
+        const product = doc.data();
+        product.id = doc.id;
+
+        newProducts.push(product);
+      });
+      // задать новый массив как состояние комапо
+      setProducts(newProducts);
+    });
+
     onAuthChange((user) => {
       setUser(user);
     });
   }, []);
+  
   return (
     <div className="App">
       <AppContext.Provider
-        value={{ categories, products, cart, setCart, user }}
+        value={{ categories, products, cart, setCart, user, orders }}
       >
         <Layout>
           <Routes>
@@ -97,6 +124,8 @@ export default function App() {
             <Route path="/category/:path" element={<Category />} />
             <Route path="/product/:path" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="thank-you" element={<ThankYou />} />
+            <Route path="/orders" element={<Orders />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
