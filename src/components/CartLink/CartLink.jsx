7a197c727a8742abff792127ec9ac083f3058910
helenenur <helenenur95@gmail.com>
2023-05-02 01:19:36 +0600

    replaced the picture of the basket

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -1,24 +1,24 @@
-import "./CartLink.css"; 
-import { useContext } from "react"; 
-import { AppContext } from "../../App"; 
-import { Link } from "react-router-dom"; 
- 
-export default function CartLink() { 
-  const { cart } = useContext(AppContext); 
- 
-  const total = Object.values(cart).reduce((acc, num) => acc + num, 0); 
- 
-  return ( 
-    <div className="CartLink"> 
-      <Link to="/cart"> 
-        <a href="/cart"> 
-          <img 
-            src="https://cdn-icons-png.flaticon.com/512/263/263142.png" 
-            alt="Cart-pht" 
-          /> 
-          {total} 
-        </a> 
-      </Link> 
-    </div> 
-  ); 
+import "./CartLink.css";
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+
+export default function CartLink() {
+  const { cart } = useContext(AppContext);
+
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
+
+  return (
+    <div className="CartLink">
+      <Link to="/cart">
+        <a href="/cart">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLdOyACPxuzmX6JaBM-E_Cn52eplbOpSjFyw&usqp=CAU"
+            alt="Cart-pht"
+          />
+          {total}
+        </a>
+      </Link>
+    </div>
+  );
 }
