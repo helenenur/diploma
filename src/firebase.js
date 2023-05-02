7f9b1fe73691d043f53dc09261fe692c03ea00aa
helenenur <helenenur95@gmail.com>
2023-05-02 13:28:11 +0600

    Cheked firebase was mistake

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {collection, getFirestore} from "firebase/firestore";
-import {GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut} from "firebase/auth";
-
+import { collection, getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -11,7 +10,7 @@ const firebaseConfig = {
   projectId: "diploma-67301",
   storageBucket: "diploma-67301.appspot.com",
   messagingSenderId: "1090921941220",
-  appId: "1:1090921941220:web:87dd19c0b3b9636a0644f1"
+  appId: "1:1090921941220:web:87dd19c0b3b9636a0644f1",
 };
 
 // Initialize Firebase
@@ -19,11 +18,12 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+// получить список категорий (коллекция документов).
 export const categoryCollection = collection(db, 'categories');
-export const productCollection = collection(db,'products');
-export const orderCollection = collection(db,'orders');
+export const productCollection = collection(db, 'products');
+export const orderCollection = collection(db, 'orders');
 
 const provider = new GoogleAuthProvider();
-export const logIn = () =>signInWithPopup(auth, provider);
-export const logOut = ()=> signOut(auth);
-export const onAuthChange = (callback)=> onAuthStateChanged(auth, callback);
+export const logIn = () => signInWithPopup(auth, provider);
+export const logOut = () => signOut(auth);
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
