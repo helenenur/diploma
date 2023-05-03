5f1b3adfabbff671451ff16237a964bd2fcfb4d5
helenenur <helenenur95@gmail.com>
2023-05-03 13:25:17 +0600

    Paste picture and some change

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
 import "./Logo.css";
-import logo from"../../assets/logo.svg";
+import logo from"../../assets/logo.jpg";
 import { NavLink } from "react-router-dom";
 
 export default function Logo(){
   return (
     <div className="Logo">
        <NavLink to="/">
-       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqjbPAzRmgjaYN8fdgE-hksAeLeRHp9G3rlA&usqp=CAU"alt="Website logo"/>
+       <img src={logo} alt="Website logo"/>
        </NavLink>
     </div>
   );
