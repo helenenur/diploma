9fdbf07db02647e54366cee83a175d12094ed0a5
helenenur <helenenur95@gmail.com>
2023-05-04 13:36:41 +0600

    Added new style

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,17 +5,27 @@ import Category from "../../pages/Category";
 import CategoryList from "../CategoryList/CategoryList";
 import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
+import NavToggle from "../NavToggle/NavToggle";
+import Drawer from "../Drawer/Drawer";
+import { useState } from "react";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="Layout">
       <header>
         <div className="head">
           <Logo />
           <Nav />
+          <NavToggle callback={toggleDrawer} />
+          <Drawer open={drawerOpen} toggle={toggleDrawer} />
+          <CartLink />
         </div>
-        <CartLink/>
-        <Auth/>
+        <Auth />
       </header>
       <aside>
         <CategoryList />
