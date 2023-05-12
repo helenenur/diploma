afb7a1de60a15d7b0bc8f2634fa9c0805f0ecccd
helenenur <helenenur95@gmail.com>
2023-05-12 12:37:58 +0600

    Added some information about company

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,18 @@
-export default function About(){
-  return(
+
+import "./About.css";
+
+export default function About() {
+  return (
     <div className="About">
-      <h1>Our company founded 1988</h1>
-      <p>We have a lot of fillials</p>
+      <h1>About us</h1>
+
+      <p>
+        Our shop is an European online platform for fashion, lifestyle and
+        sport, located in Girona (Spain). We deliver to customers in 190
+        countries. 20+ million customers around the world have trusted us. We
+        ship about 20.000 orders a day.
+      </p>
+
       <DisplayHeader />
     </div>
   );
