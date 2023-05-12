5125ac18cb3bc501606b79cc0057224fafe25841
helenenur <helenenur95@gmail.com>
2023-05-12 14:46:32 +0600

    added icon btn

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,8 +1,71 @@
+import "./Contacts.css";
+
 export default function Contacts() {
   return (
     <div className="Contacts">
       <h1>995005227</h1>
       <p>W/p/telegramm</p>
+      <div class="wrapper">
+  <div class="btn-container">
+    
+	<a href="#" class="btn btn--1">
+		<div class="content">
+			<div class="front">
+				<div class="border"></div>
+				<i class="fab fa-twitter"></i>
+			</div>
+			<div class="back">
+				<div class="border"></div>
+				<p>Twitter</p>
+			</div>
+		</div>
+	</a>
+    
+	<a href="#" class="btn btn--2">
+		<div class="content">
+			<div class="front">
+				<div class="border"></div>
+				<i class="fab fa-youtube"></i>
+			</div>
+			<div class="back">
+				<div class="border"></div>
+				<p>YouTube</p>
+			</div>
+		</div>
+	</a>
+    
+	<a href="#" class="btn btn--3">
+		<div class="content">
+			<div class="front">
+				<div class="border"></div>
+				<i class="fab fa-codepen"></i>
+			</div>
+			<div class="back">
+				<div class="border"></div>
+				<p>CodePen</p>
+			</div>
+		</div>
+	</a>
+    
+	<a href="#" class="btn btn--4">
+		<div class="content">
+			<div class="front">
+				<div class="border"></div>
+				<i class="fab fa-github"></i>
+			</div>
+			<div class="back">
+				<div class="border"></div>
+				<p>GitHub</p>
+			</div>
+		</div>
+   </a>
+    
+  </div>
+</div>
+
+{/* <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"> */}
+
+
     </div>
   );
 }
