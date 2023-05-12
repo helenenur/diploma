3e435f69c2c951f7cddeaae7941bb9588ba1dea5
helenenur <helenenur95@gmail.com>
2023-05-12 12:40:07 +0600

    tryed add some changed

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,6 +4,8 @@ import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
+import DeleteCategory from "../DeleteCategory/DeleteCategory";
+import DeleteProduct from "../DeleteProduct/DeleteProduct";
 
 
 export default function ProductList({ category }) {
@@ -19,13 +21,14 @@ export default function ProductList({ category }) {
       </Link>
       <span>{product.price} som</span>
       <AddToCart product={product} />
+      <DeleteProduct product={product}/>
     </div>
   ));
 
   return (
     <div className="ProductList">
       {output}
-      < AddProduct  />
+      < AddProduct category={category} />
     </div>
   );
 }
