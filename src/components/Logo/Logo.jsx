41ac6fc9d4c00ff348484d94ce37fa919633d773
helenenur <helenenur95@gmail.com>
2023-05-15 14:06:28 +0600

    deleted logo and changed him

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 import "./Logo.css";
-import logo from"../../assets/logo.jpg";
+import logo from "../../assets/logo.jpg";
 import { NavLink } from "react-router-dom";
 
-export default function Logo(){
+export default function Logo() {
   return (
     <div className="Logo">
-       <NavLink to="/">
-       <img src={logo} alt="Website logo"/>
-       </NavLink>
+      <NavLink to="/">
+        <img src={logo} alt="Website logo" />
+      </NavLink>
     </div>
   );
 }
