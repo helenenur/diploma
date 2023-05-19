7858102085f8b07100de3ea651f2dba082e32201
helenenur <helenenur95@gmail.com>
2023-05-19 15:32:39 +0600

    added component homelist

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import HomeList from "../components/HomeList/HomeList";
+
 export default function Home() {
   return (
     <div className="Home">
       <h1>Welcome to my Sneakers shop</h1>
-<p>We only use natural product to make our sneakers</p>
-
+      <p>We only use natural product to make our sneakers</p>
+<HomeList />
     </div>
   );
 }
