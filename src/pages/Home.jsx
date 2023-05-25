0535e18e71eef2a261828509cd63ae346e814fdc
helenenur <helenenur95@gmail.com>
2023-05-25 18:07:44 +0600

    added img for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,7 @@ export default function Home() {
   return (
     <div className="Home">
       <h1>Welcome to my Sneakers shop</h1>
-<p>We only use natural product to make our sneakers</p>
-
-
+      <p>We only use natural product to make our sneakers</p>
     </div>
   );
 }
-
