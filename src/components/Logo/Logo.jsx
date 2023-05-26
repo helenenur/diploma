d08b1ade5bab491fa829ea681f25158412cb0870
helenenur <helenenur95@gmail.com>
2023-05-26 14:59:15 +0600

    fixed my logo and renamed

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import "./Logo.css";
-import logo from "../../assets/logo.jpg";
+import logo from "../../assets/logotip.png";
 import { NavLink } from "react-router-dom";
 
 export default function Logo() {
